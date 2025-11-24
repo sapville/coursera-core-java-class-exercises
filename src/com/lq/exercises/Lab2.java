d65f9eae0b81c5a5c6a465ec8d0b9d3a82444aae
Evgenii Fedoseev <abap@sapville.com>
2025-11-24 14:31:02 -0800

    module2: primitive data types

--- src/com/lq/exercises/Lab2.java
+++ src/com/lq/exercises/Lab2.java
@@ -1,20 +1,46 @@
-/**
- * 
- */
 package com.lq.exercises;
 
-/**
- * @author developer
- *
- */
 public class Lab2 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		int width;
+        int height;
+        int area;
+        double radius = 10;
+        double pi = 3.14;
+        boolean result = true;
 
+        int[] daysInMonths = new int[12];
+        String[] monthNames = new String[] {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+        for (int i = 0; i < daysInMonths.length; i++) {
+            switch (i) {
+                case 0, 2, 4, 6, 7, 9, 11:
+                    daysInMonths[i] = 31;
+                    break;
+                case 1:
+                    daysInMonths[i] = 28;
+                    break;
+                default:
+                    daysInMonths[i] = 30;
+            }
+        }
+        width = 8;
+        height = 12;
+        area = 96;
+
+        System.out.printf("The value of width is %d%n", width);
+        System.out.printf("The value of height is %d%n", height);
+        System.out.printf("The value of area is %d%n", area);
+        System.out.printf("The value of radius is %.1f%n", radius);
+        System.out.printf("The value of pi is %.2f%n", pi);
+        System.out.printf("The value of result is %b%n", result);
+
+        System.out.println();
+
+        for (int i = 0; i < monthNames.length; i++) {
+            System.out.printf("%s has %d days.%n", monthNames[i], daysInMonths[i]);
+        }
 	}
 
 }
