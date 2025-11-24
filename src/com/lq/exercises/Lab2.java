3f1fc403fd1c6e5efe0ce3fb541cbf836f51e6ad
Eugene Fedoseev <fedoseev@sapville.com>
2025-11-24 18:28:12 +0000

    init

---
+++ src/com/lq/exercises/Lab2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.lq.exercises;
+
+/**
+ * @author developer
+ *
+ */
+public class Lab2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
