88c58b5bbab3fde95a9ff944e32f7701d8c8d0e9
Evgenii Fedoseev <abap@sapville.com>
2025-11-30 19:22:08 -0800

    p2.module 1: add tires to Car

--- src/com/lq/exercises/Car.java
+++ src/com/lq/exercises/Car.java
@@ -9,11 +9,13 @@ public class Car {
     private final String name;
     private final LocalDate manufactureDate;
     private final int maxSpeed;
+    private final int[] tires;
 
-    public Car(String name, LocalDate manufactureDate, int maxSpeed) {
+    public Car(String name, LocalDate manufactureDate, int maxSpeed, int ... tires) {
         this.name = name;
         this.manufactureDate = manufactureDate;
         this.maxSpeed = maxSpeed;
+        this.tires = tires;
     }
 
     public int getAge() {
@@ -33,13 +35,21 @@ public class Car {
         return String.format("Car %s is of %d yeas old and has maximum speed %d km/h", getName(), getAge(), getMaxSpeed());
     }
 
+    public int[] getTires() {
+        return tires;
+    }
+
     public static void main(String[] args) {
         Car[] cars = {
-                new Car("Car 1", LocalDate.of(1975, 1, 1), 120),
+                new Car("Car 1", LocalDate.of(1975, 1, 1), 120, 1000),
                 new Car("Car 2", LocalDate.of(2024, 10, 2), 220)
         };
 
         Arrays.stream(cars).forEach(System.out::println);
+        System.out.println("Car One Tires are:");
+        Arrays.stream(cars[0].getTires()).forEach(System.out::println);
+
+
     }
 
 }
